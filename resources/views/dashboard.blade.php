8ad3114e83f35e32ab1f4091c6f7a130b7a2340d
Unknown <prkjha25@gmail.com>
2018-11-04 10:28:47 -0800

    setting up dash board

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,16 +8,27 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h3>Your Listings</h3>
+                     @if(count($listings))
+                     <table class="table table-striped">
+                        <tr>
+                            <th>Company</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach($listings as $listing)
+                         <tr>
+                            <td>{{$listing->name}}</td>
+                            <td></td>
+                            <td></td>
+                         </tr>
+                        @endforeach
+                     </table>
+                     @endif          
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
