9ddc3257bbd2186965aeca99f19d456f5cf7ccb6
Unknown <prkjha25@gmail.com>
2018-11-05 11:25:26 -0800

    delete listing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,13 @@
                          <tr>
                             <td>{{$listing->name}}</td>
                             <td><a class="pull-right btn btn-default" href="/listings/{{$listing->id}}/edit">Edit</td>
-                            <td></td>
+                            <td>
+                                 {!!Form::open(['action' => ['ListingsController@destroy', $listing->id],'method' => 'POST', 'class' => 'pull-left', 'onsubmit' => 'return confirm("Are you sure?")'])!!}
+                                {{Form::hidden('_method', 'DELETE')}}
+                                {{Form::bsSubmit('Delete', ['class' => 'btn btn-danger'])}}
+                              {!! Form::close() !!}
+
+                            </td>
                          </tr>
                         @endforeach
                      </table>
